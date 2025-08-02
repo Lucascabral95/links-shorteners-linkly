7c7ed14527f9645940fd515e3aa8bbafc8ea1606
Lucascabral95 <lucassteam1995@hotmail.com>
2025-08-02 04:03:37 -0300

    fix scripts envs

--- scripts/generate-env.js
+++ scripts/generate-env.js
@@ -1,52 +1,57 @@
 const { writeFileSync, mkdirSync } = require("fs");
-require("dotenv").config();
+// require("dotenv").config();
 
-const targetPath = "./src/environments/environment.ts";
-const targetPathDev = "./src/environments/environment.development.ts";
+try {
+  const targetPath = "./src/environments/environment.ts";
+  const targetPathDev = "./src/environments/environment.development.ts";
 
-const productionEnv = process.env["PRODUCTION"];
+  const productionEnv = process.env["PRODUCTION"];
 
-if (!productionEnv) {
-  throw new Error("PRODUCTION environment variable is not defined");
+  if (!productionEnv) {
+    throw new Error("PRODUCTION environment variable is not defined");
+  }
+
+  mkdirSync("./src/environments", { recursive: true });
+
+  const envFileContent = `
+  export const environment = {
+    production: ${process.env.PRODUCTION_PROD},
+    apiUrl: "${process.env.API_URL_PROD}",
+    version: "${process.env.VERSION}",
+    darkMode: "${process.env.DARK_MODE}",
+    lightMode: "${process.env.LIGHT_MODE}",
+    STORAGE_KEY: "${process.env.STORAGE_KEY}",
+    timeSuccessModal: ${process.env.TIME_SUCCESS_MODAL},
+    STORAGE_KEY_AUTH: "${process.env.STORAGE_KEY_AUTH}",
+    SECRET_KEY_JWT: "${process.env.SECRET_KEY_JWT}",
+    TOAST_TIME: ${process.env.TOAST_TIME},
+    REDIRECT_TIME: ${process.env.REDIRECT_TIME},
+    MY_FRONTEND_URL_REDIRECT: "${process.env.MY_FRONTEND_URL_REDIRECT_PROD}",
+    DIRECT_LINK: ${process.env.DIRECT_LINK},
+    };
+    `;
+
+  const envFileContentDev = `
+    export const environment = {
+      production: ${process.env.PRODUCTION},
+      apiUrl: "${process.env.API_URL}",
+      version: "${process.env.VERSION}",
+      darkMode: "${process.env.DARK_MODE}",
+      lightMode: "${process.env.LIGHT_MODE}",
+      STORAGE_KEY: "${process.env.STORAGE_KEY}",
+      timeSuccessModal: ${process.env.TIME_SUCCESS_MODAL},
+      STORAGE_KEY_AUTH: "${process.env.STORAGE_KEY_AUTH}",
+      SECRET_KEY_JWT: "${process.env.SECRET_KEY_JWT}",
+      TOAST_TIME: ${process.env.TOAST_TIME},
+      REDIRECT_TIME: ${process.env.REDIRECT_TIME},
+      MY_FRONTEND_URL_REDIRECT: "${process.env.MY_FRONTEND_URL_REDIRECT}",
+      DIRECT_LINK: ${process.env.DIRECT_LINK},
+      };
+      `;
+
+  writeFileSync(targetPath, envFileContent);
+  writeFileSync(targetPathDev, envFileContentDev);
+} catch (error) {
+  console.error("Error generating environment files:", error);
+  process.exit(1);
 }
-
-const envFileContent = `
-export const environment = {
-  production: ${process.env.PRODUCTION_PROD},
-  apiUrl: "${process.env.API_URL_PROD}",
-  version: "${process.env.VERSION}",
-  darkMode: "${process.env.DARK_MODE}",
-  lightMode: "${process.env.LIGHT_MODE}",
-  STORAGE_KEY: "${process.env.STORAGE_KEY}",
-  timeSuccessModal: ${process.env.TIME_SUCCESS_MODAL},
-  STORAGE_KEY_AUTH: "${process.env.STORAGE_KEY_AUTH}",
-  SECRET_KEY_JWT: "${process.env.SECRET_KEY_JWT}",
-  TOAST_TIME: ${process.env.TOAST_TIME},
-  REDIRECT_TIME: ${process.env.REDIRECT_TIME},
-  MY_FRONTEND_URL_REDIRECT: "${process.env.MY_FRONTEND_URL_REDIRECT_PROD}",
-  DIRECT_LINK: ${process.env.DIRECT_LINK},
-};
-`;
-
-const envFileContentDev = `
-export const environment = {
-  production: ${process.env.PRODUCTION},
-  apiUrl: "${process.env.API_URL}",
-  version: "${process.env.VERSION}",
-  darkMode: "${process.env.DARK_MODE}",
-  lightMode: "${process.env.LIGHT_MODE}",
-  STORAGE_KEY: "${process.env.STORAGE_KEY}",
-  timeSuccessModal: ${process.env.TIME_SUCCESS_MODAL},
-  STORAGE_KEY_AUTH: "${process.env.STORAGE_KEY_AUTH}",
-  SECRET_KEY_JWT: "${process.env.SECRET_KEY_JWT}",
-  TOAST_TIME: ${process.env.TOAST_TIME},
-  REDIRECT_TIME: ${process.env.REDIRECT_TIME},
-  MY_FRONTEND_URL_REDIRECT: "${process.env.MY_FRONTEND_URL_REDIRECT}",
-  DIRECT_LINK: ${process.env.DIRECT_LINK},
-};
-`;
-
-mkdirSync("./src/environments", { recursive: true });
-
-writeFileSync(targetPath, envFileContent);
-writeFileSync(targetPathDev, envFileContentDev);
